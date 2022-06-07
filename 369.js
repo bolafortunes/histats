a1844bcbd071e318247e5a4c16f4d7b1cdceb884
DMCA <68084740+bolafortunes@users.noreply.github.com>
2022-06-07 21:00:57 +0700

    369.js

---
+++ 369.js
@@ -0,0 +1,9 @@
+&lt;script type=&#39;text/javascript&#39;&gt;
+//&lt;![CDATA[
+$(document).ready(function(){
+ $(&quot;#btnSubmit&quot;).click(function(){
+        $(&quot;.asu&quot;).css(&quot;background&quot;, &quot;&quot;); //passing value as null.
+        $(&quot;.asuballhk&quot;).css(&quot;background&quot;, &quot;&quot;); //passing value as null.
+         $(&quot;.singa&quot;).css(&quot;background&quot;, &quot;&quot;); //passing value as null.
+    }); 
+}); 
--- README.md
+++
@@ -1,2 +0,0 @@
-# histats
-Clear
